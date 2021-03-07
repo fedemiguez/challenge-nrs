18853f38ed59fe2b37b1da0372166d79446b2272
Fede Miguez <fedeemiguez@gmail.com>
2021-03-07 17:54:56 -0300

    se agrega solucion de versionado en el css por el mismo problema del commit anterior pero en el css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <link href="{{asset('css/app.css?ver=1.0')}}" rel="stylesheet">
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
     <body>
